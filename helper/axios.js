c14283a69e68fd08a69dfefb819c0a64a2cd79c7
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-04-09 07:30:33 -0300

    feat: create fetchData on axios helper

---
+++ helper/axios.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const fetchData = async (url) => {
+  let response;
+
+  await axios.get(url)
+    .then((res) => {
+      response = res.data;
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log('fetchData error: ', err);
+    });
+
+  return response;
+};
+
+export {
+  fetchData,
+};
